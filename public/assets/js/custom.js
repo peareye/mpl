3b4c7b87b7aa8bcf90c2564366ee8377dbdbaf05
Wolfgang Moritz <wolfmoritz@gmail.com>
2021-08-01 10:34:27 -0700

    Added custom JS to add/remove header scroll class.

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -1,1 +1,17 @@
-// Custom JS
+// Set height (in px), numbers only
+const scrollRemoveClass = 25
+const scrollAddClass = 100;
+const headerScrollClass = "jsHeaderScroll";
+
+const headerElement = document.querySelector("header.topHeader");
+window.onscroll = function () {
+    let top = document.documentElement.scrollTop;
+
+    if (top <= scrollRemoveClass) {
+        headerElement.classList.remove(headerScrollClass);
+    }
+
+    if (top >= scrollAddClass) {
+        headerElement.classList.add(headerScrollClass);
+    }
+}
